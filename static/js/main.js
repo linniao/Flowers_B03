00129717cd10b15c25c6fb51a36721f3f21b5044
linniao <18202982630@163.com>
2019-07-10 17:37:21 +0800

    zhangzhen

--- static/js/main.js
+++ static/js/main.js
@@ -11,31 +11,40 @@ function show_orign(source, target) {
     var data = new FormData();
     data.append('file', img);
 
+
     $.ajax({
         url: '/receive',
         type: 'POST',
         dataType: 'JSON',
         data: data,
+
         processData: false,//用于对data参数进行序列化处理 这里必须false
         contentType: false,
         success:function (res) {
+
             $('#orign_id').attr("width", 256);
             $('#orign_id').attr("height", 256);
+            // $('#orign_id').attr("src", res['filename1']+"?Math.random()");
             var org = document.getElementById('orign_id');
-            org.src = res['filename1'];
+            org.src = res['filename1']+"?t="+Math.random();
 
             $('#res1_id').attr("width", 256);
             $('#res1_id').attr("height", 256);
             var res_1 = document.getElementById('res1_id');
-            res_1.src = res['filename2'];
+            res_1.src = res['filename2']+"?t="+Math.random();
 
             $('#res2_id').attr("width", 256);
             $('#res2_id').attr("height", 256);
             var res_2 = document.getElementById('res2_id');
-            res_2.src = res['filename3'];
+            res_2.src = res['filename3']+"?t="+Math.random();
+
+
         },
         error: function (e) {
            alert(e);
         }
-    })
+    });
+
+
+
 }
